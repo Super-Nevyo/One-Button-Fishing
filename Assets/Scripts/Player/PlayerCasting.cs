d8d4701f4b24896afe605264c3a59188a4aa025c
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-06-09 18:29:12 -0600

    boundaries!!
    boundary check, got it set up to two gameobjects, so it can be manipulated, and fucked w. might need to be refactored later but we ball for now

--- Assets/Scripts/Player/PlayerCasting.cs
+++ Assets/Scripts/Player/PlayerCasting.cs
@@ -7,18 +7,21 @@ public class PlayerCasting : MonoBehaviour
     [SerializeField] private float targetSpeed;
     [SerializeField] private float aimMaxTimer;
 
+    [SerializeField] private Transform boundaryPoint1;
+    [SerializeField] private Transform boundaryPoint2;
+
     private float aimTimer;
 
     private void Update()
     {
         aimTarget.position += playerBody.transform.up * (targetSpeed * Time.deltaTime);
-        
-        if(aimTimer > aimMaxTimer)
-        {
+
+        if (aimTarget.position.x <= boundaryPoint1.position.x || aimTarget.position.x >= boundaryPoint2.position.x)
             targetSpeed *= -1;
-            aimTimer = 0; 
-        }
         
-        aimTimer += Time.deltaTime;
+        if(aimTarget.position.y <= boundaryPoint1.position.y || aimTarget.position.y >= boundaryPoint2.position.y) 
+            targetSpeed *= -1;
+        
+        
     }
 }
